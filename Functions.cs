312558da3c059c8d699fe5878c837cf8e394359c
Kinza <35830797+Kinza-Sarwar@users.noreply.github.com>
2018-01-26 20:21:51 +1300

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,195 @@
+﻿using RestSharp;
+using SendLog_Console.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SendLog_Console.PostModel;
+using System.Net.Http;
+using System.Diagnostics;
+using System.IO;
+using System.Configuration;
+using SendLog_Console.Logging;
+using Newtonsoft.Json;
+using MPhillAssignment.LogData;
+
+namespace SendLog_Console
+{
+    public static class Functions
+    {
+        private static string ExecutionLogFile = ConfigurationManager.AppSettings["ExecutionLogFile_Path"].ToString();
+        private static string LogFile = ConfigurationManager.AppSettings["LogFile_Path"].ToString();
+
+        public static KeyValuePair<string, T[]> PairedWith<T>(this string key, T[] value)
+        {
+            return new KeyValuePair<string, T[]>(key, value);
+        }
+
+        public static KeyValuePair<string, T> PairedWith<T>(this string key, T value)
+        {
+            return new KeyValuePair<string, T>(key, value);
+        }
+
+        public static void Log(object value = null)
+        {
+            Console.WriteLine(value);
+        }
+
+        public static void Write(string text)
+        {
+            Console.Write(text);
+        }
+
+        public static string Read()
+        {
+            return Console.ReadLine();
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
+        }
+
+        public static async Task<AccessToken> Login(string username, string password)
+        {
+            Login_PostModel data = new Login_PostModel { GrantType = "password", UserName = username.ToLower(), Password = password };
+            IRestResponse<string> response = await new Services().ExecuteService<string, Login_PostModel>(Method.POST, Url.Login, data, HttpHeaders.FormUrlEncoded, false);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<AccessToken>(response.Data);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<string> Register(string username, string password)
+        {
+            IRestResponse<string> response = await new Services().ExecuteService<string, int>(Method.GET, string.Format("{0}/{1}/{2}", Url.Register, username, password));
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return response.Data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<T> CallService<T, Q>(Method method, string resource, bool authenticate = true, params KeyValuePair<string, Q>[] query) where T : new()
+        {
+            IRestResponse<string> response = await new Services().ExecuteService<string, Q>(method, resource, authenticate, query);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<T>(response.Data);
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public static async Task<T> CallService<T, Q>(Method method, string resource, Q data, string httpHeaders = "", bool authenticate = true) where T : new()
+        {
+            try
+            {
+                IRestResponse<T> response = await new Services().ExecuteService<T, Q>(method, resource, data, httpHeaders, authenticate);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return response.Data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static string GetTimeString(this Stopwatch stopwatch, int numberofDigits = 1)
+        {
+            double time = stopwatch.ElapsedTicks / (double)Stopwatch.Frequency;
+            if (time > 1)
+                return Math.Round(time, numberofDigits) + " s";
+            if (time > 1e-3)
+                return Math.Round(1e3 * time, numberofDigits) + " ms";
+            if (time > 1e-6)
+                return Math.Round(1e6 * time, numberofDigits) + " µs";
+            if (time > 1e-9)
+                return Math.Round(1e9 * time, numberofDigits) + " ns";
+            return stopwatch.ElapsedTicks + " ticks";
+        }
+
+        private static void Log(string functionName, string time, TextWriter w)
+        {
+            w.Write("\r\nLog Entry:\t");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("Function Name:\t{0}", functionName);
+            w.WriteLine("Execution Time:\t{0}", time);
+            w.WriteLine("--------------------------------------------------------------");
+        }
+
+        public static void LogExecutionTime(string functionName, Stopwatch sWatch)
+        {
+            //if (!File.Exists(ExecutionLogFile))
+            //{
+            //    var stream = File.Create(ExecutionLogFile);
+            //    stream.Close();
+            //}
+            using (StreamWriter w = File.CreateText(ExecutionLogFile))
+            {
+                Log(functionName, sWatch.GetTimeString(), w);
+            }
+        }
+
+        public static void Save_ApiLogData(List<Log_Data> data)
+        {
+            using (StreamWriter w = File.CreateText(LogFile))
+            {
+                foreach (var item in data)
+                {
+                    Log_Data d = item.DecryptData(Encryption.PublicKey);
+                    d.LogData_Id = Encryption.EncryptText(d.LogData_Id.ToString(), Encryption.PublicKey);
+                    string jsonData = JsonConvert.SerializeObject(d);
+                    w.Write(jsonData);
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,366 @@
+﻿using MPhillAssignment.LogData;
+using Newtonsoft.Json;
+using RestSharp;
+using SendLog_Console.Logging;
+using SendLog_Console.PostModel;
+using SendLog_Console.Service;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using C = SendLog_Console.Functions;
+
+namespace SendLog_Console
+{
+    class Program
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+        static bool success = false;
+        static bool LoggedIn = false;
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Clear();
+                do
+                {
+                    Console.Clear();
+                    C.Log("==================================================================");
+                    C.Log("1. User Login:");
+                    C.Log("2. Register New User:");
+                    C.Log();
+                    C.Log("0. Exit");
+                    C.Log("==================================================================");
+                    C.Write("Enter your Choice:\t");
+                    string choice = C.Read();
+                    int userChoice = 0;
+                    if (int.TryParse(choice, out userChoice))
+                    {
+                        C.Log();
+                        C.Log("==================================================================");
+                        switch (userChoice)
+                        {
+                            case 0:
+                                Environment.Exit(0);
+                                break;
+                            case 1:
+                                UserLogin();
+                                break;
+                            case 2:
+                                RegisterUser();
+                                break;
+                            default:
+                                C.Log("You have entered a wrong choice. Please try again.");
+                                break;
+                        }
+                        C.Log();
+                        C.Log("Press any key to continue.................");
+                        C.Read();
+                    }
+                    else
+                    {
+                        C.Log("You have entered a wrong choice. Please try again.");
+                        C.Log();
+                        C.Log("Press any key to continue.................");
+                        C.Read();
+                    }
+                    Console.Clear();
+                } while (!LoggedIn);
+
+                //UserLogin();
+                if (success)
+                {
+                    success = true;
+                    Console.Clear();
+                    do
+                    {
+                        C.Log("==================================================================");
+                        C.Log("1. Save Log file to Logging Server:");
+                        C.Log("2. Get Log file from Logging Server:");
+                        C.Log("3. Update Log file to Logging Server:");
+                        C.Log();
+                        C.Log("0. Exit");
+                        C.Log("==================================================================");
+                        C.Write("Enter your Choice:\t");
+                        string choice = C.Read();
+                        int userChoice = 0;
+                        if (int.TryParse(choice, out userChoice))
+                        {
+                            C.Log();
+                            C.Log("==================================================================");
+                            switch (userChoice)
+                            {
+                                case 0:
+                                    Environment.Exit(0);
+                                    break;
+                                case 1:
+                                    SaveFile();
+                                    break;
+                                case 2:
+                                    GetLogFile();
+                                    break;
+                                case 3:
+                                    UpdateFile();
+                                    break;
+                                default:
+                                    C.Log("You have entered a wrong choice. Please try again.");
+                                    break;
+                            }
+                            C.Log();
+                            C.Log("Press any key to continue.................");
+                            C.Read();
+                        }
+                        else
+                        {
+                            C.Log("You have entered a wrong choice. Please try again.");
+                            C.Log();
+                            C.Log("Press any key to continue.................");
+                            C.Read();
+                        }
+                        Console.Clear();
+                    } while (true);
+                }
+                C.Read();
+            }
+            catch (Exception ex)
+            {
+                C.Log("============================ERROR============================");
+                C.Log(ex.Message);
+                C.Read();
+            }
+        }
+
+        private static void UserLogin()
+        {
+            AccessToken userData = null;
+            do
+            {
+                success = true;
+                C.Write("Username:\t");
+                string userName = C.Read();
+                C.Write("Password:\t");
+                string password = C.ReadPassword();
+
+                stopwatch.Restart();
+                //AccessToken data = C.CallService<AccessToken, object>(Method.POST, Url.Login, login, HttpHeaders.FormUrlEncoded, false).Result;
+                userData = C.Login(userName, password).Result;
+                stopwatch.Stop();
+                C.LogExecutionTime("User Login", stopwatch);
+
+                if (userData != null)
+                {
+                    C.Log("Login Successfull");
+                    success = true;
+                }
+                else
+                {
+                    C.Log("Username or Password is incorrect");
+                    success = false;
+                    C.Read();
+                }
+                C.Log();
+                C.Log("========================================================");
+                C.Log();
+            } while (!success);
+
+            if (success)
+            {
+                if (userData.IsActive)
+                {
+                    Services.AccessToken = userData.Access;
+                    Encryption.PublicKey = userData.PublicKey;
+                    LoggedIn = true;
+                }
+                else
+                {
+                    success = false;
+                    Services.AccessToken = userData.Access;
+                    C.Log();
+                    C.Log("========================================================");
+                    C.Log("You have been deactived from the system.");
+                    C.Log("========================================================");
+                }
+            }
+        }
+
+        private static void SaveFile()
+        {
+            do
+            {
+                success = true;
+                C.Write("Enter log file Path:\t");
+                string fileName = C.Read();
+
+                StreamReader re = File.OpenText(fileName);
+                string fileData = re.ReadToEndAsync().Result;
+
+                if (fileData.Substring(fileData.IndexOf("}") + 1, 1) != ",")
+                {
+                    fileData = "[" + fileData.Replace("}", "},") + "]";
+                }
+
+                List<Log_Data> data = JsonConvert.DeserializeObject<List<Log_Data>>(fileData);
+                C.Log("Saving Log to Logging Server");
+                List<Log_Data> eData = new List<Log_Data>();
+
+                stopwatch.Restart();
+                foreach (var item in data)
+                {
+                    var d = item.EncryptData(Encryption.PublicKey);
+                    eData.Add(d);
+                }
+                stopwatch.Stop();
+                C.LogExecutionTime("SaveFile: Encrypting File", stopwatch);
+
+                stopwatch.Restart();
+                bool saveData = C.CallService<bool, List<Log_Data>>(Method.POST, Url.SaveLog, eData, HttpHeaders.Json, true).Result;
+                stopwatch.Stop();
+                C.LogExecutionTime("SaveFile: Uploading File", stopwatch);
+                if (saveData)
+                {
+                    success = true;
+                    C.Log();
+                    C.Log("========================================================");
+                    C.Log("Log Uploaded Successfully");
+                    C.Log("========================================================");
+                    C.Log("========================================================");
+                }
+                else
+                {
+                    success = false;
+                    C.Log("========================================================");
+                    C.Log("Error: Please try again.");
+                    C.Log("========================================================");
+                    C.Read();
+                }
+            } while (!success);
+        }
+
+        private static void GetLogFile()
+        {
+            do
+            {
+                success = true;
+                C.Write("Please Enter Username :\t");
+                string userEmail = C.Read();
+
+                stopwatch.Restart();
+                ApiData<List<Log_Data>> logData = C.CallService<ApiData<List<Log_Data>>, string>(Method.GET, Url.GetLog, true, "email".PairedWith(userEmail)).Result;
+                stopwatch.Stop();
+                C.LogExecutionTime("GetLogFile: Download File", stopwatch);
+                if (logData.Status)
+                {
+                    success = true;
+                    stopwatch.Restart();
+                    C.Save_ApiLogData(logData.Data);
+                    stopwatch.Stop();
+                    C.LogExecutionTime("GetLogFile: Save File", stopwatch);
+                    C.Log();
+                    C.Log("========================================================");
+                    C.Log("Log Saved Successfully");
+                    C.Log("========================================================");
+                    C.Log("========================================================");
+                }
+                else
+                {
+                    success = false;
+                    C.Log("========================================================");
+                    C.Log(logData.Message);
+                    C.Log("========================================================");
+                    C.Read();
+                }
+            } while (!success);
+        }
+
+        private static void UpdateFile()
+        {
+            do
+            {
+                success = true;
+                C.Write("Enter log file Path:\t");
+                string fileName = C.Read();
+
+                StreamReader re = File.OpenText(fileName);
+                string fileData = re.ReadToEndAsync().Result;
+
+                if (fileData.Substring(fileData.IndexOf("}") + 1, 1) != ",")
+                {
+                    fileData = "[" + fileData.Replace("}", "},") + "]";
+                }
+
+                List<Log_Data> data = JsonConvert.DeserializeObject<List<Log_Data>>(fileData);
+                C.Log("Saving Log to Logging Server");
+                List<Log_Data> eData = new List<Log_Data>();
+
+                stopwatch.Restart();
+                foreach (var item in data)
+                {
+                    var d = item.EncryptData(Encryption.PublicKey);
+                    d.LogData_Id = Encryption.DecryptText(d.LogData_Id, Encryption.PublicKey);
+                    eData.Add(d);
+                }
+                stopwatch.Stop();
+                C.LogExecutionTime("UpdateFile: Encrypt File", stopwatch);
+
+                stopwatch.Restart();
+                bool saveData = C.CallService<bool, List<Log_Data>>(Method.POST, Url.UpdateLog, eData, HttpHeaders.Json, true).Result;
+                stopwatch.Stop();
+                C.LogExecutionTime("UpdateFile: Upload File", stopwatch);
+                if (saveData)
+                {
+                    success = true;
+                    C.Log();
+                    C.Log("========================================================");
+                    C.Log("Log Uploaded Successfully");
+                    C.Log("========================================================");
+                    C.Log("========================================================");
+                }
+                else
+                {
+                    success = false;
+                    C.Log("========================================================");
+                    C.Log("Error: Please try again.");
+                    C.Log("========================================================");
+                    C.Read();
+                }
+            } while (!success);
+        }
+
+        private static void RegisterUser()
+        {
+            do
+            {
+                success = true;
+                C.Write("Username:\t");
+                string userName = C.Read();
+                C.Write("Password:\t");
+                string password = C.ReadPassword();
+
+                stopwatch.Restart();
+                //AccessToken data = C.CallService<AccessToken, object>(Method.POST, Url.Login, login, HttpHeaders.FormUrlEncoded, false).Result;
+                string registerData = C.Register(userName, password).Result;
+                stopwatch.Stop();
+                C.LogExecutionTime("Register new user", stopwatch);
+
+                if (!string.IsNullOrEmpty(registerData))
+                {
+                    C.Log(registerData);
+                    success = true;
+                }
+                else
+                {
+                    C.Log(registerData);
+                    success = false;
+                }
+                C.Log();
+                C.Log("========================================================");
+                C.Log();
+            } while (!success);
+        }
+
+    }
+}
